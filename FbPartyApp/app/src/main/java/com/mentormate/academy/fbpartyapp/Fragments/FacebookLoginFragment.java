e3e93aa1ef820d5872aa91d2f472a95b74990d91
ivaylokostov <ivaylokostov@gmail.com>
2015-02-10 19:47:11 +0200

    FB Login
    Commit working FB login (added session listeners and UiLifecycleHelper
    in FB fragment)

--- FbPartyApp/app/src/main/java/com/mentormate/academy/fbpartyapp/Fragments/FacebookLoginFragment.java
+++ FbPartyApp/app/src/main/java/com/mentormate/academy/fbpartyapp/Fragments/FacebookLoginFragment.java
@@ -2,13 +2,20 @@ package com.mentormate.academy.fbpartyapp.Fragments;
 
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.facebook.Session;
+import com.facebook.SessionState;
+import com.facebook.UiLifecycleHelper;
+import com.facebook.widget.LoginButton;
+import com.mentormate.academy.fbpartyapp.Fragments.Utils.Constants;
 import com.mentormate.academy.fbpartyapp.R;
 
 /**
@@ -16,10 +23,77 @@ import com.mentormate.academy.fbpartyapp.R;
  */
 public class FacebookLoginFragment extends Fragment {
 
-    @Nullable
+    private UiLifecycleHelper uiHelper;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.activity_main, container, false);
+
+        LoginButton authButton = (LoginButton) view.findViewById(R.id.authButton);
+        authButton.setFragment(this);
+
         return view;
     }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        uiHelper = new UiLifecycleHelper(getActivity(), callback);
+        uiHelper.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        // For scenarios where the main activity is launched and user
+        // session is not null, the session state change notification
+        // may not be triggered. Trigger it if it's open/closed.
+        Session session = Session.getActiveSession();
+        if (session != null &&
+                (session.isOpened() || session.isClosed()) ) {
+            onSessionStateChange(session, session.getState(), null);
+        }
+
+        uiHelper.onResume();
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        uiHelper.onActivityResult(requestCode, resultCode, data);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        uiHelper.onPause();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        uiHelper.onDestroy();
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        uiHelper.onSaveInstanceState(outState);
+    }
+
+    private void onSessionStateChange(Session session, SessionState state, Exception exception) {
+        if (state.isOpened()) {
+            Log.i(Constants.LOG_DEBUG, "Logged in...");
+        } else if (state.isClosed()) {
+            Log.i(Constants.LOG_DEBUG, "Logged out...");
+        }
+    }
+
+    private Session.StatusCallback callback = new Session.StatusCallback() {
+        @Override
+        public void call(Session session, SessionState state, Exception exception) {
+            onSessionStateChange(session, state, exception);
+        }
+    };
 }
--- FbPartyApp/app/src/main/java/com/mentormate/academy/fbpartyapp/MainActivity.java
+++ FbPartyApp/app/src/main/java/com/mentormate/academy/fbpartyapp/MainActivity.java
@@ -1,8 +1,12 @@
 package com.mentormate.academy.fbpartyapp;
 
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
 import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Base64;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -12,6 +16,9 @@ import com.facebook.widget.LoginButton;
 import com.mentormate.academy.fbpartyapp.Fragments.FacebookLoginFragment;
 import com.mentormate.academy.fbpartyapp.Fragments.Utils.Constants;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 
 public class MainActivity extends FragmentActivity {
 
@@ -20,7 +27,27 @@ public class MainActivity extends FragmentActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //setContentView(R.layout.activity_main);
+
+
+        //IF you get an error: "Invalid keyhash"
+        // Uncomment below lines and look for "KeyHash:" in Logcat.
+        // Copy that keyhash in the FB app settings;
+        /*
+        try {
+            PackageInfo info = getPackageManager().getPackageInfo(
+                    "com.mentormate.academy.fbpartyapp",
+                    PackageManager.GET_SIGNATURES);
+            for (Signature signature : info.signatures) {
+                MessageDigest md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                Log.d("KeyHash:", Base64.encodeToString(md.digest(), Base64.DEFAULT));
+            }
+        } catch (PackageManager.NameNotFoundException e) {
+
+        } catch (NoSuchAlgorithmException e) {
+
+        }*/
+
 
         if (savedInstanceState == null) {
             // Add the fragment on initial activity setup
