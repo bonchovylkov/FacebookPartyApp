2e0185c1eeb3480d7739b052d16a46cfa5740efd
ivaylokostov <ivaylokostov@gmail.com>
2015-02-15 23:41:41 +0200

    + DBhelper

---
+++ FbPartyApp/app/src/main/java/com/mentormate/academy/fbpartyapp/Events/DBHelper.java
@@ -0,0 +1,45 @@
+package com.mentormate.academy.fbpartyapp.Events;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+import com.mentormate.academy.fbpartyapp.Utils.Constants;
+
+/**
+ * Created by ivaylok on 2/15/2015.
+ */
+    public class DBHelper extends SQLiteOpenHelper {
+
+
+
+    private final static String CREATE_TABLE_SQL = "" +
+            "CREATE TABLE " + Constants.TABLE_NAME +
+            " ( " +
+                Constants.DB_ID  + " INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, " +
+                Constants.DB_EVENT_ID + " BIGINT NOT NULL, " +
+                Constants.DB_URL + " TEXT NOT NULL" +
+            " );"
+            ;
+
+    public DBHelper(Context context) {
+        super(context, Constants.DB_DBNAME, null, Constants.DB_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+        db.execSQL(CREATE_TABLE_SQL);
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        Log.d(Constants.LOG_DEBUG, "DB upraged from " + oldVersion + " to version " + newVersion);
+        db.execSQL("DROP TABLE IF EXISTS " + Constants.TABLE_NAME);
+        onCreate(db);
+    }
+
+
+}
